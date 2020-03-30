2cdcf2779909b0e83dd0f85c42ee976ae96ac2d0
Harishchandra Pal <jr.harishchandra@gmail.com>
2020-03-30 23:22:37 +0530

    New comments added

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practice_project.settings')  # can be used for adding new file outside of dir as scraper.py
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
